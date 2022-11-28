87119aa40ffb7d0a7f458b48b6d5a015edd551aa
Gabriel Hondet <7418676+gabrielhdt@users.noreply.github.com>
2022-11-28 12:04:55 +0100

    Add Nix flakes for both packages

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "Build environment based on callCabal2nix";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+      hpkgs = pkgs.haskellPackages;
+      simple-smt-z3 = hpkgs.callCabal2nix "simple-smt-z3" ./Z3 {};
+      simple-smt = hpkgs.callCabal2nix "simple-smt" ./. {};
+    in {
+      formatter = pkgs.alejandra;
+      devShells = let
+        buildInputs = with pkgs; [cabal-install z3 haskell-language-server];
+      in {
+        default = pkgs.mkShell {
+          inputsFrom = [simple-smt.env];
+          inherit buildInputs;
+        };
+        z3 = pkgs.mkShell {
+          inputsFrom = [simple-smt-z3.env];
+          inherit buildInputs;
+        };
+      };
+    });
+}
