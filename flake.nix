68f13bb0a03189d13d83be5575ddbed9cab5cd4b
Gabriel Hondet <7418676+gabrielhdt@users.noreply.github.com>
2022-11-30 14:23:36 +0100

    Something else

--- flake.nix
+++ flake.nix
@@ -10,16 +10,17 @@
       pkgs = nixpkgs.legacyPackages.${system};
       hpkgs = pkgs.haskellPackages;
       simple-smt-z3 = hpkgs.callCabal2nix "simple-smt-z3" ./Z3 {};
-      simple-smt = hpkgs.callCabal2nix "simple-smt" ./. {};
+      # simple-smt = hpkgs.callCabal2nix "simple-smt" ./. {};
     in {
       formatter = pkgs.alejandra;
       devShells = let
-        buildInputs = with pkgs; [cabal-install z3 haskell-language-server ormolu];
+        buildInputs =
+          with pkgs; [cabal-install z3 haskell-language-server ormolu];
       in {
-        default = pkgs.mkShell {
-          inputsFrom = [simple-smt.env];
-          inherit buildInputs;
-        };
+        # default = pkgs.mkShell {
+        #   inputsFrom = [simple-smt.env];
+        #   inherit buildInputs;
+        # };
         z3 = pkgs.mkShell {
           inputsFrom = [simple-smt-z3.env];
           inherit buildInputs;
