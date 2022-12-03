20ccf89c90ed0c5b08db684692aadeef77cb2e91
Gabriel Hondet <7418676+gabrielhdt@users.noreply.github.com>
2022-12-03 20:41:10 +0100

    Add shellhook for libz3

--- flake.nix
+++ flake.nix
@@ -11,32 +11,41 @@
     nixpkgs,
     flake-utils,
   }:
-    flake-utils.lib.eachSystem ["x86_64-linux"] (system: let
-      pkgs = nixpkgs.legacyPackages.${system};
-      hpkgs = pkgs.haskellPackages;
-      simple-smt = hpkgs.callCabal2nix "simple-smt" ./. {};
-      simple-smt-tests = hpkgs.callCabal2nix "simple-smt-tests" ./tests {};
-      simple-smt-z3 = hpkgs.callCabal2nix "simple-smt-z3" ./Z3 {
-        inherit simple-smt-tests;
-      };
-    in {
-      formatter = pkgs.alejandra;
-      devShells = {
-        default = hpkgs.shellFor {
-          packages = p: [
-            simple-smt
-            simple-smt-tests
-            simple-smt-z3
-          ];
-          withHoogle = true;
-          buildInputs =
-            (with hpkgs; [
-              cabal-install
-              hlint
-              haskell-language-server
-            ])
-            ++ [pkgs.z3];
+    with flake-utils.lib;
+      eachSystem [system.x86_64-linux] (system: let
+        pkgs = nixpkgs.legacyPackages.${system};
+        hpkgs = pkgs.haskellPackages;
+        simple-smt = hpkgs.callCabal2nix "simple-smt" ./. {};
+        simple-smt-tests = hpkgs.callCabal2nix "simple-smt-tests" ./tests {};
+        simple-smt-z3 = hpkgs.callCabal2nix "simple-smt-z3" ./Z3 {
+          inherit simple-smt-tests;
         };
-      };
-    });
+      in {
+        formatter = pkgs.alejandra;
+
+        devShells = {
+          default = hpkgs.shellFor {
+            packages = p: [
+              simple-smt
+              simple-smt-tests
+              simple-smt-z3
+            ];
+
+            withHoogle = true;
+
+            buildInputs =
+              (with hpkgs; [
+                cabal-install
+                hlint
+                haskell-language-server
+              ])
+              ++ [pkgs.z3];
+
+            ## Needed by the haskell-language-server
+            shellHook = ''
+              export LD_LIBRARY_PATH="${pkgs.z3.lib}/lib:''${LD_LIBRARY_PATH:+:}"
+            '';
+          };
+        };
+      });
 }
